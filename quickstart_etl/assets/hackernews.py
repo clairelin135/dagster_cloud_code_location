61450fb3f415283406f2287ea8f738e0f903f933
Claire Lin <claire@dagsterlabs.com>
2024-01-02 14:23:20 -0800

    add backfill policy assets

--- quickstart_etl/assets/hackernews.py
+++ quickstart_etl/assets/hackernews.py
@@ -13,36 +13,55 @@ from dagster import (
     asset,
     HourlyPartitionsDefinition,
     BackfillPolicy,
+    DailyPartitionsDefinition,
 )
+
+
+@asset(
+    partitions_def=DailyPartitionsDefinition(start_date="2023-01-01"),
+    backfill_policy=BackfillPolicy.single_run(),
+    group_name="with_backfill_policy",
+)
+def successful_daily_asset() -> MaterializeResult:
+    return MaterializeResult()
+
+
+@asset(
+    partitions_def=DailyPartitionsDefinition(start_date="2023-01-01"),
+    backfill_policy=BackfillPolicy.single_run(),
+    group_name="with_backfill_policy",
+)
+def failing_daily_asset() -> None:
+    raise Exception("This asset failed")
 
 
 @asset(
     partitions_def=HourlyPartitionsDefinition(start_date="2023-01-01-00:00"),
     backfill_policy=BackfillPolicy.single_run(),
 )
-def hourly_2023():
-    pass
+def hourly_2023() -> MaterializeResult:
+    return MaterializeResult()
 
 
 @asset(
     partitions_def=HourlyPartitionsDefinition(start_date="2021-01-01-00:00"),
     backfill_policy=BackfillPolicy.single_run(),
 )
-def hourly_2021():
-    pass
+def hourly_2021() -> MaterializeResult:
+    return MaterializeResult()
 
 
 @asset(
     partitions_def=HourlyPartitionsDefinition(start_date="2019-01-01-00:00"),
     backfill_policy=BackfillPolicy.single_run(),
 )
-def hourly_2019():
-    pass
+def hourly_2019() -> MaterializeResult:
+    return MaterializeResult()
 
 
 @asset(
     partitions_def=HourlyPartitionsDefinition(start_date="2018-01-01-00:00"),
     backfill_policy=BackfillPolicy.single_run(),
 )
-def hourly_2018():
-    pass
+def hourly_2018() -> MaterializeResult:
+    return MaterializeResult()
