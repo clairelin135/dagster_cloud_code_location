93dc9369bd4491eaa824c4768ae75066c371dfa1
Claire Lin <claire@dagsterlabs.com>
2024-04-16 14:11:52 -0700

    change to use dagster-cloud

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(exclude=["quickstart_etl_tests"]),
     install_requires=[
         "dagster==1.7.2rc2",
-        "dagster-plus==1.7.2rc2",
+        "dagster-cloud==1.7.2rc2",
         "boto3",
         "pandas",
         "matplotlib",
