f707ee8bcb52f2524e4a921352cc61ca3bf5b337
Claire Lin <claire@dagsterlabs.com>
2024-04-18 15:29:12 -0700

    use user code version 1.6.0

--- quickstart_etl/assets/hackernews.py
+++ quickstart_etl/assets/hackernews.py
@@ -13,7 +13,7 @@ from dagster import (
     BackfillPolicy,
     DailyPartitionsDefinition,
 )
-from dagster_cloud.dagster_insights import dbt_with_bigquery_insights
+from dagster_cloud.instance import DagsterCloudAgentInstance
 
 
 @asset(
@@ -32,35 +32,3 @@ def successful_daily_asset() -> MaterializeResult:
 )
 def failing_daily_asset() -> None:
     raise Exception("This asset failed")
-
-
-@asset(
-    partitions_def=HourlyPartitionsDefinition(start_date="2023-01-01-00:00"),
-    backfill_policy=BackfillPolicy.single_run(),
-)
-def hourly_2023() -> MaterializeResult:
-    return MaterializeResult()
-
-
-@asset(
-    partitions_def=HourlyPartitionsDefinition(start_date="2021-01-01-00:00"),
-    backfill_policy=BackfillPolicy.single_run(),
-)
-def hourly_2021() -> MaterializeResult:
-    return MaterializeResult()
-
-
-@asset(
-    partitions_def=HourlyPartitionsDefinition(start_date="2019-01-01-00:00"),
-    backfill_policy=BackfillPolicy.single_run(),
-)
-def hourly_2019() -> MaterializeResult:
-    return MaterializeResult()
-
-
-@asset(
-    partitions_def=HourlyPartitionsDefinition(start_date="2018-01-01-00:00"),
-    backfill_policy=BackfillPolicy.single_run(),
-)
-def hourly_2018() -> MaterializeResult:
-    return MaterializeResult()
