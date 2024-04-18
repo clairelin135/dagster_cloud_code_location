9e287a63e3a1efce3a5bd234aed9267b72629174
Claire Lin <claire@dagsterlabs.com>
2024-04-18 15:40:57 -0700

    nevermind

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="quickstart_etl",
     packages=find_packages(exclude=["quickstart_etl_tests"]),
     install_requires=[
-        "dagster==1.6.0",
+        "dagster==1.7.2rc3",
         "dagster-cloud==1.7.2rc3",
         "boto3",
         "pandas",
